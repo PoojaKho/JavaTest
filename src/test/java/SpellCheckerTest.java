183b3649dccaf010f417bef41226b8b66eac0fb1
poojakhot <pooja.a.bhardwaj@gmail.com>
2019-06-05 23:03:55 -0500

    added tests

--- src/test/java/SpellCheckerTest.java
+++ src/test/java/SpellCheckerTest.java
@@ -3,15 +3,19 @@ import org.junit.Test;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.apache.commons.io.FileUtils;
 
+import static org.junit.Assert.assertTrue;
+
 public class SpellCheckerTest {
 
-    private void readDictionary(String word) {
+    private List<String> readDictionary(String word) {
+        List<String> spellCheck = new ArrayList<>();
         try {
             Optional<URL> url = Optional.ofNullable(getClass().getClassLoader().getResource("US.dic"));
             if (url.isPresent()) {
@@ -19,34 +23,35 @@ public class SpellCheckerTest {
                 if (file.exists()) {
                     List<String> spellingList = FileUtils.readLines(file, "UTF-8");
                     List<String> filteredList = spellingList.stream().filter(v -> v.contains(word)).collect(Collectors.toList());
-
                     if (filteredList.size() != 0) {
-                        Optional<String> spelling1 = filteredList.stream().filter(v -> v.startsWith(word)).findFirst();
-
-                        if (spelling1.isPresent()) {
-                            if (spelling1.get().equalsIgnoreCase(word)) {
-                                System.out.println("word exist in the dictionary: " + word);
-                            } else {
-                                System.out.println("Do you mean any of the following words?");
-                                System.out.println(filteredList.stream().collect(Collectors.joining(", ")));
-                            }
+                        List<String> newFilteredListMatchedWords = filteredList.stream().filter(v -> v.equalsIgnoreCase(word)).collect(Collectors.toList());
+                        if (newFilteredListMatchedWords.size() > 0) {
+                            System.out.println("word exist in the dictionary: " + word);
+                            spellCheck.add(word);
+                        } else {
+                            System.out.println("Do you mean any of the following words?");
+                            System.out.println(filteredList.stream().collect(Collectors.joining(", ")));
+                            spellCheck.addAll(filteredList.stream().collect(Collectors.toList()));
                         }
                     }
                 }
             }
+
         } catch (NullPointerException | IOException ex) {
             System.out.println(ex.getMessage());
         }
-
+        return spellCheck;
     }
 
     @Test
     public void testCorrectCheckSpellingAndSuggestOptions() {
-        readDictionary("tes");
+        assertTrue(readDictionary("tes").size() > 0);
     }
 
     @Test
-    public void testCheckIfWordExistsInDictionary(){
-        readDictionary("test");
+    public void testCheckIfWordExistsInDictionary() {
+        List<String> result=readDictionary("test");
+        assertTrue(result.size() == 1);
+        assertTrue(result.get(0).equalsIgnoreCase("test"));
     }
 }
